baa6de145554fe25ecf5ad6d93d759cb69c8f810
Yonatan Karp-Rudin <yonatan.karp@sumup.com>
2023-06-09 17:48:51 +0200

    Remove acceptance test
    This commit removes the AT as they've been replaced by the DDT tests

--- src/test/kotlin/com/yonatankarp/zettai/at/Actions.kt
+++
@@ -1,9 +0,0 @@
-package com.yonatankarp.zettai.at
-
-import com.yonatankarp.zettai.domain.ToDoList
-
-typealias Step = Actions.() -> Unit
-
-interface Actions {
-    fun getToDoList(user: String, listName: String): ToDoList?
-}

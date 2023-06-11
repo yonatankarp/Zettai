47f3589a7f051f6039359ed0bebe2728b1e233b0
Yonatan Karp-Rudin <yonatan.karp@sumup.com>
2023-06-11 21:31:08 +0200

    Improve error handling for get todo list
    This commit improves the error handling for the Get DOTO list endpoint, which would return 404 in case the list or the user doesn't exist

--- src/main/kotlin/com/yonatankarp/zettai/utils/Functional.kt
+++ src/main/kotlin/com/yonatankarp/zettai/utils/Functional.kt
@@ -8,6 +8,13 @@ typealias FUN<A, B> = (A) -> B
 infix fun <A, B, C> FUN<A, B>.andThen(f: FUN<B, C>): FUN<A, C> = { a: A -> f(this(a)) }
 
 /**
+ * Composes two functions together if the value is not null, otherwise will return null without
+ * continuing the calculation.
+ */
+infix fun <A : Any, B : Any, C : Any> FUN<A, B?>.andUnlessNull(other: FUN<B, C?>): FUN<A, C?> =
+    { a: A -> this(a)?.let { other(it) } }
+
+/**
  * Applies the given function to the sequence if it is not null or return null if it does.
  */
 fun <U : Any> CharSequence?.unlessNullOrEmpty(f: (CharSequence) -> U): U? =
@@ -17,4 +24,8 @@ fun <U : Any> CharSequence?.unlessNullOrEmpty(f: (CharSequence) -> U): U? =
 /**
  * A debug utility function to print the value of a variable.
  */
-fun <T> T.printIt(prefix: String = ">"): T = also{ println("$prefix $this") }
+fun <T> T.printIt(prefix: String = ">"): T = also { println("$prefix $this") }
+
+fun <T> T.discardUnless(predicate: (T) -> Boolean): T? = if (predicate(this)) this else null
+
+fun <A, B, C> ((A, B) -> C).curry(): (A) -> (B) -> C = { a: A -> { b: B -> this(a, b) } }

fa94974c7fda8db6c8ef0ded646d7283706d5fcb
Yonatan Karp-Rudin <yonatan.karp@sumup.com>
2023-06-18 09:50:23 +0200

    Implement Outcome functor
    This commit adds the `Outcome` functor that would be used in the project to handle errors in code. This class is used for educational purposes only, and would be replaced later on by Either from ArrowKT library

---
+++ src/main/kotlin/com/yonatankarp/zettai/utils/Outcome.kt
@@ -0,0 +1,42 @@
+package com.yonatankarp.zettai.utils
+
+import com.yonatankarp.zettai.utils.Outcome.Failure
+import com.yonatankarp.zettai.utils.Outcome.Success
+
+sealed class Outcome<out E : OutcomeError, out T> {
+    data class Success<T> internal constructor(val value: T) : Outcome<Nothing, T>()
+    data class Failure<E : OutcomeError> internal constructor(val error: E) : Outcome<E, Nothing>()
+
+    fun <U> transform(f: (T) -> U): Outcome<E, U> =
+        when (this) {
+            is Success -> f(value).asSuccess()
+            is Failure -> this
+        }
+
+    fun <T, E : OutcomeError> Outcome<E, T>.recover(recoverError: (E) -> T): T =
+        when (this) {
+            is Success -> value
+            is Failure -> recoverError(error)
+        }
+
+    companion object {
+        fun <T, U, E : OutcomeError> lift(f: (T) -> U): (Outcome<E, T>) -> Outcome<E, U> =
+            { o -> o.transform { f(it) } }
+    }
+}
+
+interface OutcomeError {
+    val msg: String
+}
+
+inline fun <T, E : OutcomeError> Outcome<E, T>.onFailure(exitBlock: (E) -> Nothing): T =
+    when (this) {
+        is Success<T> -> value
+        is Failure<E> -> exitBlock(error)
+    }
+
+fun <E : OutcomeError> E.asFailure(): Outcome<E, Nothing> = Failure(this)
+fun <T> T.asSuccess(): Outcome<Nothing, T> = Success(this)
+
+fun <T : Any, E : OutcomeError> T?.failIfNull(error: E): Outcome<E, T> =
+    this?.asSuccess() ?: error.asFailure()
